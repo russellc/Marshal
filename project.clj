e4e47c21a288399c69e54ccea88537f763f86ef0
Russell Christopher <russell.christopher@gmail.com>
2012-02-12 09:32:07 -0500

    bump version to 1.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject marshal "1.0.0"
+(defproject marshal "1.1.0"
   :description "Marshal clojure data structures to legacy binary protocols"
   :url "http://github.com/russellc/Marshal"
   :dependencies [[org.clojure/clojure "1.3.0"]])
